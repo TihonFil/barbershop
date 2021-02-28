a21532d9a850ba0edd456a995ae2c98e31ce68b6
Tihon <tihon-rabota@mai.ru>
2021-02-28 18:20:56 +0300

    js burger

--- js/burger.js
+++ js/burger.js
@@ -1,10 +1,31 @@
 const button = document.querySelector('.header-burger__btn');
 const nav = document.querySelector('.header__nav');
+const links = document.querySelectorAll('.header__nav-link');
+
+const body = document.querySelector('body');
+const overlay = document.querySelector('.header-burger__overlay');
 
 const burgerBtn = function () {
-   button.addEventListener('click', function () {
+   button.addEventListener('click', function (e) {
+      e.preventDefault();
+      body.classList.toggle('lock');
       nav.classList.toggle('header__nav-active');
    });
 }
-
 burgerBtn();
+
+const linkClick = function () {
+   links.addEventListener('click', function () {
+      nav.classList.remove('header__nav-active');
+      body.classList.remove('lock');
+   });
+}
+linkClick();
+
+const overlayClick = function () {
+   overlay.addEventListener('click', function ()  {
+      body.classList.toggle('lock');
+      nav.classList.toggle('header__nav-active');
+   });
+}
+overlayClick();
